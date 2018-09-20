0274442172466e13b044bfc57ac78a23a9b3fb0b
vivek-faldu <vfaldu@asu.edu>
2018-09-20 12:24:53 -0700

    movie fetch api integration

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('this is a response');
 });
 
 module.exports = router;
