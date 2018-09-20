d94bf1c259594d4c7dea329b78c4ee26e12d3622
saran1856 <skandim2@asu.edu>
2018-09-19 18:23:41 -0700

    Added comments to 'movie-card.component.ts', 'movie.model.ts' and 'movie.service.ts'

--- Client/src/app/Components/MovieComponents/movie-card-component/movie-card.component.ts
+++ Client/src/app/Components/MovieComponents/movie-card-component/movie-card.component.ts
@@ -8,7 +8,11 @@ import { Movie } from '../../movie.model';
 })
 
 
-
+/*
+* A class that acts as controller for movie-card-component.
+* @Author: Sai Saran Kandimalla.
+* @version 1: created on 09/18/2018.
+*/
 export class MovieCardComponent implements OnInit {
 
   @Input() public movie: Movie;
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import { RouterModule, Routes } from '@angular/router';
-
 import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
