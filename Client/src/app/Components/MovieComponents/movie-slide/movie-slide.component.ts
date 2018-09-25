7eb1d762dd4253008238b9b5e2d556cb3630c4ac
saran1856 <skandim2@asu.edu>
2018-09-25 10:28:47 -0700

    added an initial version of movie slider

--- Client/src/app/Components/MovieComponents/movie-slide/movie-slide.component.ts
+++ Client/src/app/Components/MovieComponents/movie-slide/movie-slide.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieSlideComponent implements OnInit {
 
+  
+  
+  popularMovies: {sNumber: number, imageUrl: string}[] = [
+    {sNumber: 1 , imageUrl: "https://3.bp.blogspot.com/-1hXvJeWAWGY/WUDHSFbd4HI/AAAAAAAAAOc/9NvGkXxRGlERV8VSWtw7-CVw-YYTfmsKACLcBGAs/s1600/A153L.jpg"},
+  ];
+  
+  currentMovie: {sNumber: number, imageUrl: string}=this.popularMovies.find(x=>x.sNumber===1);
   constructor() { }
 
   ngOnInit() {
