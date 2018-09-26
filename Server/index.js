9e3a5e609e37aadac764908345de82ea776ba9be
shipabhat92 <sgbhat2@asu.edu>
2018-09-26 14:34:40 -0700

    fix: adding cross origin request support

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var cookieParser = require("cookie-parser");
 var indexRouter = require("./routes");
+var cors = require('cors')
 
 var bluebird = require("bluebird");
 const keys = require("./config/keys");
@@ -12,6 +13,7 @@ app.listen(PORT, () => {
   console.log("Connected to port:" + PORT);
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
