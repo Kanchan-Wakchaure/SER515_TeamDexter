4598093f1aab92e216b2243f734c9e5a04849bab
saran1856 <skandim2@asu.edu>
2018-09-29 20:20:35 -0700

    moved movie cards to home component and added route outlet in app component

--- Client/src/app/Components/home/home.component.ts
+++ Client/src/app/Components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../movie.model';
+import { MovieService } from '../../Services/movie.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public movies: Movie[];
+
+  constructor(public movieService: MovieService) { }
 
   ngOnInit() {
-  }
+    this.movieService.getMovies().subscribe(
+      
+     (response: Movie[])=> {
+       console.log(response);
+       this.movies = response;
+       console.log(this.movies[0]);
+     }
+   
+   );
+   
+   }
 
 }
