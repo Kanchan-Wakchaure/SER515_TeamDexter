e5e06bc8d4e5bbd2d9da79eae752a5ca793c123e
HP <kanchanw17@gmail.com>
2018-09-30 01:15:23 -0700

    Movie Details Page and Routing updated

--- Client/src/app/Components/movie-details/movie-details.component.ts
+++ Client/src/app/Components/movie-details/movie-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../Services/movie.service';
+import { Movie } from '../../Components/movie.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
+  public movie: Movie;
+  private movie_id: number;
+  constructor(public movieService: MovieService, private route: ActivatedRoute) { 
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
-
-
+  ngOnInit() {
+    this.route.params.subscribe(params=>{this.movie_id=params['movie_id']});
+    this.movieService.getMovieDetails(this.movie_id).subscribe((response: any)=>{
+     debugger;
+      this.movie = response;
+    });
+      }
 }
