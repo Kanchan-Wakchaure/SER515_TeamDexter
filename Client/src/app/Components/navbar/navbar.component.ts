1aced6c4b55ea5adaf596140ebbc46f4846a2c16
shipabhat92 <sgbhat2@asu.edu>
2018-10-06 01:48:25 -0700

    feat(Search movie) : User Story:55,

--- Client/src/app/Components/navbar/navbar.component.ts
+++ Client/src/app/Components/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '../../../../node_modules/@angular/material';
 
 import { LoginComponent } from '../login/login.component';
+import { FormControl } from '@angular/forms';
+import { MovieService } from '../../Services/movie.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,23 +12,33 @@ import { LoginComponent } from '../login/login.component';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  seachForm = new FormControl('');
+  constructor(public dialog: MatDialog, private movieService: MovieService) { }
 
   ngOnInit() {
   }
 
   //opens pop up when login is clicked.
   openDialog() {
-    const dialogReference = this.dialog.open(LoginComponent,{
+    const dialogReference = this.dialog.open(LoginComponent, {
       width: '600px'
     })
-    
+
     //action needed after dialog is closed.
-    dialogReference.afterClosed().subscribe(result=>{
+    dialogReference.afterClosed().subscribe(result => {
       console.log("dialog was closed");
     });
 
     return false;
   }
-    
+
+  searchMovies(movieName: string) {
+    this.movieService.getSearchedMovieList(movieName).subscribe(
+      (res: any) => {
+        debugger;
+        let movies = res;
+      }
+    );
+  }
+
 }
--- Client/src/app/Services/movie.service.ts
+++ Client/src/app/Services/movie.service.ts
@@ -1,30 +1,36 @@
-import {Movie} from '../Components/movie.model';
+import { Movie } from '../Components/movie.model';
 import { Injectable } from '../../../node_modules/@angular/core';
-import { HttpClient } from '../../../node_modules/@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '../../../node_modules/@angular/common/http';
+// import { timingSafeEqual } from 'crypto';
 
 
 /*
 * A service Class that acts as centralized repository for handling movie data.
 * @Author: Sai Saran Kandimalla.
+* @Author: Shilpa Bhat
 * @version 1: created on 09/18/2018.
 */
 @Injectable()
 export class MovieService {
 
-  //Sample data for movies.
-  public movies: Movie[]=[];   
-  
-  constructor(private http: HttpClient) { }
+    //Sample data for movies.
+    public movies: Movie[] = [];
 
-  /* A getter method to get movies.
-  * @returns movies: Movie[].
-  */
-  getMovies(){    
-    return this.http.get("http://localhost:4241/",{responseType:'json'}); 
-  }
-  
-  /* A getter method to get movie details */
-  getMovieDetails(id: number){
-    return this.http.get("http://localhost:4241/movie/"+id,{responseType:'json'});
-  }
+    constructor(private http: HttpClient) { }
+
+    /* A getter method to get movies.
+    * @returns movies: Movie[].
+    */
+    getMovies() {
+        return this.http.get("http://localhost:4241/", { responseType: 'json' });
+    }
+
+    /* A getter method to get movie details */
+    getMovieDetails(id: number) {
+        return this.http.get("http://localhost:4241/movie/" + id, { responseType: 'json' });
+    }
+
+    getSearchedMovieList(movieName: string) {
+        return this.http.get('http://localhost:4241/movies/?type=search&name=' + movieName);
+    }
 }
