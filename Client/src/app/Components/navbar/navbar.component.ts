8cf25777610bb77b666fe76f78fca7227617e4b2
HP <kanchanw17@gmail.com>
2018-10-06 18:07:40 -0700

    Signup UI created

--- Client/src/app/Components/navbar/navbar.component.ts
+++ Client/src/app/Components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '../../../../node_modules/@angular/material';
 
 import { LoginComponent } from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
 import { FormControl } from '@angular/forms';
 import { MovieService } from '../../Services/movie.service';
 
@@ -29,6 +30,15 @@ export class NavbarComponent implements OnInit {
       console.log("dialog was closed");
     });
 
+    return false;
+  }
+
+    //opens pop up when sign up button is clicked
+  openSignUp(){
+    const dialogReference = this.dialog.open(SignupComponent,{width: '600px'})
+    
+    //action needed after dialog is closed.
+    dialogReference.afterClosed().subscribe(result=>{console.log("dialog was closed");});
     return false;
   }
 
