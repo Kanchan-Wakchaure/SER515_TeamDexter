1f5f1af39726dd70c48107aadbb016685d4590f4
saran1856 <skandim2@asu.edu>
2018-10-08 14:19:09 -0700

    add backend functionality for log in

---
+++ Server/config/passport.js
@@ -0,0 +1,34 @@
+var passport = require('passport');
+var localStrategy = require('passport').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+},
+function(username,password, done) {
+    User.findOne({email: username},function(error,user) {
+        
+        if(error) {
+            return done(error);
+        }
+
+        //return if email doesn't exist
+        if(!user) {
+            return done(null,false,{
+                message: 'user not found'
+            });
+        }
+
+        //return if password is wrong
+        if(!User.validPassword(password)) {
+            return done(null,false,{
+                message: 'password is wrong'
+            });
+        }
+
+        //returns user object if credentials are correct
+        return done(null,user);
+    })
+}
+));
