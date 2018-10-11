65bb03a01f6e89e81211ec9c366c645bcb7d1ef7
Shaunak Shah <smshah7@asu.edu>
2018-10-11 01:13:20 -0700

    Preferences UI: input fields and model binding User Story: #54

---
+++ Client/src/app/Components/preferences.model.ts
@@ -0,0 +1,9 @@
+export class Preferences {
+
+	constructor(
+		public email: string,
+		public genres: string,
+		public languages: string,
+		public actors: string
+	){}
+}
--- Client/src/app/Components/preferences/preferences.component.ts
+++ Client/src/app/Components/preferences/preferences.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Preferences } from '../preferences.model';
+
 @Component({
   selector: 'app-preferences',
   templateUrl: './preferences.component.html',
   styleUrls: ['./preferences.component.css']
 })
 export class PreferencesComponent implements OnInit {
+
+  model = new Preferences('abc@example.com', 'some genre', 'some language', 'some actor');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  get diagnostic() { return JSON.stringify(this.model); }
 
   constructor() { }
 
