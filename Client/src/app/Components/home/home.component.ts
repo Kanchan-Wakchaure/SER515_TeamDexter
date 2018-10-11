3ef8c99c6c0f2ff9e7905d51888e5d68bc718da9
jananit2411 <jthiaga1@asu.edu>
2018-10-11 11:51:51 -0700

    feat:US#56 Task#59 UI update to show list of  new movies in Coming soon tab

--- Client/src/app/Components/home/home.component.ts
+++ Client/src/app/Components/home/home.component.ts
@@ -1,36 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router'
-import { Movie } from '../movie.model';
-import { MovieService } from '../../Services/movie.service';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Movie } from "../movie.model";
+import { MovieService } from "../../Services/movie.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
   public movies: Movie[];
 
-  constructor(public movieService: MovieService, private router: Router,
-    private activatedRoute: ActivatedRoute) { }
+  constructor(
+    public movieService: MovieService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    if (this.router.url.includes('/search')) {
+    if (this.router.url.includes("/search")) {
       this.searchMovies();
+    } else if (this.router.url.includes("/coming_soon")) {
+      this.upcomingMovies();
     } else {
-      this.movieService.getMovies().subscribe(
-        (response: Movie[]) => {
-          this.movies = response;
-        }
-      );
+      this.movieService.getMovies().subscribe((response: Movie[]) => {
+        this.movies = response;
+      });
     }
   }
 
   private searchMovies() {
     this.activatedRoute.params.subscribe(params => {
-      let movieName: string = params['name'];
-      this.movieService.getSearchedMovieList(movieName).subscribe((res: Movie[]) => {
+      let movieName: string = params["name"];
+      this.movieService
+        .getSearchedMovieList(movieName)
+        .subscribe((res: Movie[]) => {
+          debugger;
+          this.movies = res;
+        });
+    });
+  }
+
+  private upcomingMovies() {
+    this.activatedRoute.params.subscribe(params => {
+      this.movieService.getUpcomingMovieList().subscribe((res: Movie[]) => {
         debugger;
         this.movies = res;
       });
--- Client/src/app/Services/movie.service.ts
+++ Client/src/app/Services/movie.service.ts
@@ -1,8 +1,11 @@
-import { Movie } from '../Components/movie.model';
-import { Injectable } from '../../../node_modules/@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '../../../node_modules/@angular/common/http';
+import { Movie } from "../Components/movie.model";
+import { Injectable } from "../../../node_modules/@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams
+} from "../../../node_modules/@angular/common/http";
 // import { timingSafeEqual } from 'crypto';
-
 
 /*
 * A service Class that acts as centralized repository for handling movie data.
@@ -12,25 +15,34 @@ import { HttpClient, HttpHeaders, HttpParams } from '../../../node_modules/@angu
 */
 @Injectable()
 export class MovieService {
+  //Sample data for movies.
+  public movies: Movie[] = [];
 
-    //Sample data for movies.
-    public movies: Movie[] = [];
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) { }
-
-    /* A getter method to get movies.
+  /* A getter method to get movies.
     * @returns movies: Movie[].
     */
-    getMovies() {
-        return this.http.get("http://localhost:4241/", { responseType: 'json' });
-    }
+  getMovies() {
+    return this.http.get("http://localhost:4241/", { responseType: "json" });
+  }
 
-    /* A getter method to get movie details */
-    getMovieDetails(id: number) {
-        return this.http.get("http://localhost:4241/movie/" + id, { responseType: 'json' });
-    }
+  /* A getter method to get movie details */
+  getMovieDetails(id: number) {
+    return this.http.get("http://localhost:4241/movie/" + id, {
+      responseType: "json"
+    });
+  }
 
-    getSearchedMovieList(movieName: string) {
-        return this.http.get('http://localhost:4241/movies/?type=search&name=' + movieName);
-    }
+  getSearchedMovieList(movieName: string) {
+    return this.http.get(
+      "http://localhost:4241/movies/?type=search&name=" + movieName
+    );
+  }
+
+  getUpcomingMovieList() {
+    return this.http.get("http://localhost:4241/movies/?type=upcoming", {
+      responseType: "json"
+    });
+  }
 }
