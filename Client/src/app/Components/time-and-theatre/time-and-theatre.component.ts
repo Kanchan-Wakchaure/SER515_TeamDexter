cca48d68b0b503ca1651470c200534171bd42ec1
Shaunak <sms228@gmail.com>
2018-10-12 20:27:06 -0700

    MovieDetail UI: Connecting with backend data. User Story #53

--- Client/src/app/Components/time-and-theatre/time-and-theatre.component.ts
+++ Client/src/app/Components/time-and-theatre/time-and-theatre.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../Services/movie.service';
+import { Movie } from '../movie.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-time-and-theatre',
@@ -6,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./time-and-theatre.component.css']
 })
 export class TimeAndTheatreComponent implements OnInit {
+  
+  private movie_id: number;
+  public movie: Movie;
+  private show_detail;
+  private cinema_detail;
 
   day1 = [
   	{ time: '11:00', theatre: 'inox 1' },
   	{ time: '13:30', theatre: 'pvr 1' },
   	{ time: '14:45', theatre: 'big 1' },
-  	{ time: '17:00', theatre: 'net 1' },
+  	{ time: '17:00', theatre: 'net 1' }
 
   ];
   
@@ -19,7 +27,7 @@ export class TimeAndTheatreComponent implements OnInit {
   	{ time: '11:20', theatre: 'inox 2' },
   	{ time: '13:50', theatre: 'pvr 2' },
   	{ time: '14:00', theatre: 'big 2' },
-  	{ time: '17:05', theatre: 'net 2' },
+  	{ time: '17:05', theatre: 'net 2' }
 
   ];
 
@@ -27,12 +35,35 @@ export class TimeAndTheatreComponent implements OnInit {
   	{ time: '11:10', theatre: 'inox 3' },
   	{ time: '13:40', theatre: 'pvr 3' },
   	{ time: '14:10', theatre: 'big 3' },
-  	{ time: '17:10', theatre: 'net 3' },
+  	{ time: '17:10', theatre: 'net 3' }
 
   ];
 
-  constructor() { }
+  constructor(public movieService: MovieService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.route.params.subscribe(params => { this.movie_id = params['movie_id'] });
+    this.movieService.getMovieDetails(this.movie_id).subscribe((response: any) => {
+      this.show_detail = response.show_detail;
+      this.cinema_detail = response.cinema_detail;
+    });
+    
   }
+
+  get diagnostic() { return JSON.stringify(this.show_detail); }
+
+  attachCinema(show_detail, cinema_detail){
+    for(let show of show_detail){
+      let tempTheatre = "";
+      for(let cinema of cinema_detail){
+        if(cinema.id == show.id){
+          tempTheatre = cinema.name;
+          break;
+        }
+      }
+      this.day1.push(time: show.time, theatre: tempTheatre);
+    }
+  }
+
 }
