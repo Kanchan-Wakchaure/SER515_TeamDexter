213c7f148962cbc1c231329b700a009098dc7262
jananit2411 <jthiaga1@asu.edu>
2018-10-24 09:30:53 -0700

    fix: compile error

--- Client/src/app/Components/activation/activation.component.ts
+++ Client/src/app/Components/activation/activation.component.ts
@@ -1,37 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../../Services/authentication.service';
-import 'rxjs/add/operator/filter';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../../Services/authentication.service";
+import "rxjs/add/operator/filter";
 @Component({
-  selector: 'app-activation',
-  templateUrl: './activation.component.html',
-  styleUrls: ['./activation.component.css']
+  selector: "app-activation",
+  templateUrl: "./activation.component.html"
+  //styleUrls: ["./activation.component.css"]
 })
 export class ActivationComponent implements OnInit {
   code: string;
   successmessage: string;
   redirectmessage: string;
-  constructor(private route: ActivatedRoute, private authenticationService: AuthenticationService, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.route.queryParams
-      .filter(params => params.code)
-      .subscribe(params => {
-        console.log(params); // {order: "popular"}
+    this.route.queryParams.filter(params => params.code).subscribe(params => {
+      console.log(params); // {order: "popular"}
 
-        this.code = params.code;
-        console.log(this.code); // popular
-        this.authenticationService.activate(this.code).subscribe((response: any) => {
+      this.code = params.code;
+      console.log(this.code); // popular
+      this.authenticationService
+        .activate(this.code)
+        .subscribe((response: any) => {
           console.log(response);
           this.successmessage = response.message;
-          this.redirectmessage = 'redirecting you to home page....';
+          this.redirectmessage = "redirecting you to home page....";
           setTimeout(() => {
-            this.router.navigate(['/home']);
+            this.router.navigate(["/home"]);
           }, 2000);
         });
-      });
+    });
   }
-
-
 }
