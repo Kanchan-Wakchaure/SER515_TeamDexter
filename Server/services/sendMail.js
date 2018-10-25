b4fff12e6c46b8b963fb74617e072de13d16e6b2
jananit2411 <jthiaga1@asu.edu>
2018-10-24 23:09:09 -0700

    feat : #US92 Update to send email notifications

--- Server/services/sendMail.js
+++ Server/services/sendMail.js
@@ -1,33 +1,29 @@
 var nodemailer = require('nodemailer');
+var mailer = require('../services/mailer');
 const UserPreference = require('../models/UserPreference');
-var mailer = require('../services/mailer')
 
 module.exports.sendEmail = function (req, res) {
-    var emailList = fecthEmails();
-    console.log(emailList);
-    for (var i = 0; i < emailList.length; i++) {
-        var mailOptions = {
-            from: 'findmyshow1@gmail.com',
-            to: emailList[i],
-            subject: 'Find My Show - Notifications',
-            html: '<body><h1>Hello User</h1><p>Here are your list of movies for this week</p></body>'
-        };
-        mailer.sendEmail(mailOptions);
-    }
+    fetchEmails().then(function (emailList) {
+        for (var i = 0; i < emailList.length; i++) {
+            var mailOptions = {
+                from: 'findmyshow1@gmail.com',
+                to: emailList[i],
+                subject: 'Find My Show - Notifications',
+                html: '<body><p>Hello User,Here are your list of movies for this week</p></body>'
+            };
+            mailer.sendEmail(mailOptions);
+        }
+    });
 }
 
-function fecthEmails() {
+function fetchEmails() {
     var listOfEmails = [];
-    UserPreference.find((err, preferences) => {
-        if (err)
-            return next(err);
-        else
-            for (var x = 0; x < preferences.length; x++) {
-                console.log(preferences[x].email);
-                //var str = (String)(preferences[x].email);
-                //str.trim();
-                //listOfEmails.push(str);
-            }
+    var query = UserPreference.find();
+    return query.exec().then(function (preferences) {
+        for (var x = 0; x < preferences.length; x++) {
+            var str = (String)(preferences[x].email);
+            listOfEmails.push(str);
+        }
+        return listOfEmails;
     })
-    return listOfEmails;
 }
