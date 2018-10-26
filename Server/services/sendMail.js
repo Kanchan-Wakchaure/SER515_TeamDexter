660c44f521045ad55c1be9174fa84c15613230f4
jananit2411 <jthiaga1@asu.edu>
2018-10-25 20:27:38 -0700

    feat: US#92 Task#100,101 - Update to email body with just static data like preferred genre list

--- Server/services/sendMail.js
+++ Server/services/sendMail.js
@@ -3,27 +3,43 @@ var mailer = require('../services/mailer');
 const UserPreference = require('../models/UserPreference');
 
 module.exports.sendEmail = function (req, res) {
-    fetchEmails().then(function (emailList) {
-        for (var i = 0; i < emailList.length; i++) {
-            var mailOptions = {
-                from: 'findmyshow1@gmail.com',
-                to: emailList[i],
-                subject: 'Find My Show - Notifications',
-                html: '<body><p>Hello User,Here are your list of movies for this week</p></body>'
-            };
-            mailer.sendEmail(mailOptions);
+    var emailList = [];
+    fetchID().then(function (IdList) {
+        for (var i = 0; i < 1; i++) {
+            fetchPreferences(IdList[i]).then(function (preferences) {
+                var email = preferences.email;
+                var myPreferences = preferences.genreList;
+                emailList.push(email);
+                var mailOptions = {
+                    from: 'findmyshow1@gmail.com',
+                    to: email,
+                    subject: 'Find My Show - Notifications',
+                    html: '<body><h1>Hello User!</h1><p>Here are your list of movies for this week</p><div>' + myPreferences + '</div></html>'
+                };
+                mailer.sendEmail(mailOptions);
+            });
         }
     });
+    res.json("Emails Sent Successfully");
 }
 
-function fetchEmails() {
-    var listOfEmails = [];
+function fetchID() {
+    var listOfIDs = [];
     var query = UserPreference.find();
     return query.exec().then(function (preferences) {
         for (var x = 0; x < preferences.length; x++) {
-            var str = (String)(preferences[x].email);
-            listOfEmails.push(str);
+            listOfIDs.push(preferences[x].id);
         }
-        return listOfEmails;
+        return listOfIDs;
+    })
+}
+
+function fetchPreferences(myID) {
+    var details = new UserPreference();
+    var query = UserPreference.findById(myID);
+    return query.exec().then(function (preferences) {
+        details.email = preferences.email;
+        details.genreList = preferences.genreList;
+        return details;
     })
 }
