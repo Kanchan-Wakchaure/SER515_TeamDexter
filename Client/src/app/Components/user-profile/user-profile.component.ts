884c32436037b0284cd93ac4e21f245f664ae454
Kanchan <kanchanw17@gmail.com>
2018-10-29 18:13:39 -0700

    US#90 Task#106 User profile edit mode UI

--- Client/src/app/Components/user-profile/user-profile.component.ts
+++ Client/src/app/Components/user-profile/user-profile.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { AuthenticationService } from '../../Services/authentication.service';
 import { FormGroup, FormControl, Validators } from '../../../../node_modules/@angular/forms';
+import { City } from '../city.model';
+import { CityService } from '../../Services/city.service';
+import { PreferenceService } from '../../Services/preference.service';
+import { Preference } from '../../Components/preference.model';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,12 +15,21 @@ import { FormGroup, FormControl, Validators } from '../../../../node_modules/@an
 export class UserProfileComponent implements OnInit {
   profile: any;
   userprofileForm: FormGroup;
-  fnameholder: string;
-  lnameholder: string;
-  emailholder: string;
-  locationholder: string;
+  readonly: boolean=true;
+  
+  public cities: City[];
+
+  
+  public preference: Preference;
+  genreList = [];
+  languageList = [];
+  actorsList = [];
+
+  dropdownSettings = {};
 
   constructor(private router: Router,
+              private cityService: CityService,
+              private preferenceService: PreferenceService,
               private authService: AuthenticationService) { }
 
   ngOnInit() {
@@ -29,10 +42,58 @@ export class UserProfileComponent implements OnInit {
       })
     });
     this.profile =  this.authService.getUser();
-    this.fnameholder = this.profile.firstName;
-    this.lnameholder = this.profile.lastname;
-    this.emailholder = this.profile.email; 
-    this.locationholder = this.profile.location;
+
+    this.cityService.getCities().subscribe((response: City[]) => {
+      this.cities = response; } );
+
+      this.preference = new Preference();
+      this.genreList = [
+        { id: 1, item_text: 'Action' },
+        { id: 2, item_text: 'Drama' },
+        { id: 3, item_text: 'Romance' },
+        { id: 4, item_text: 'Thriller' },
+        { id: 5, item_text: 'Comedy' }
+      ];
+  
+      this.languageList = [
+        { id: 1, item_text: 'English' },
+        { id: 2, item_text: 'Spanish' },
+        { id: 3, item_text: 'Hindi' }
+      ];
+  
+      this.actorsList = [
+        { id: 1, item_text: 'Will Smith' },
+        { id: 2, item_text: 'Tom Hanks' },
+        { id: 3, item_text: 'Angelina Jolie' },
+        { id: 4, item_text: 'Tom Cruise' }
+      ];
+  
+      this.dropdownSettings = {
+        singleSelection: false,
+        idField: 'id',
+        textField: 'item_text',
+        selectAllText: 'Select All',
+        unSelectAllText: 'UnSelect All',
+        itemsShowLimit: 3,
+        allowSearchFilter: true,
+      };
+
+      this.getPreference();
+  }
+
+  editProfile(){
+    this.readonly = false;
+  }
+
+  saveChanges(){
+    this.readonly = true;
+    
+  }
+
+  getPreference(){
+    this.preferenceService.getPreferencesByEmail(this.profile.email).subscribe((response: any) => {
+      this.preference = response;
+    });
   }
 
 }
