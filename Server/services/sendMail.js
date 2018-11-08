db19d288f284c9936f150fdfa93c3b2f00da6854
jananit2411 <jthiaga1@asu.edu>
2018-11-08 15:46:44 -0700

    feat : US #2 Task 152 - Update to notification module to fetch details from User model

--- Server/services/sendMail.js
+++ Server/services/sendMail.js
@@ -1,14 +1,23 @@
 var nodemailer = require('nodemailer');
 var mailer = require('../services/mailer');
-const UserPreference = require('../models/UserPreference');
+const User = require('../models/User');
 
 module.exports.sendEmail = function (req, res) {
     var emailList = [];
     fetchID().then(function (IdList) {
         for (var i = 0; i < IdList.length; i++) {
-            fetchPreferences(IdList[i]).then(function (preferences) {
-                var email = preferences.email;
-                var myPreferences = preferences.genreList;
+            fetchPreferences(IdList[i]).then(function (users) {
+                var email = users.email;
+                var firstName = users.firstname;
+                var lastName = users.lastname;
+                var myGenreList = [];
+                var myActorList = [];
+                for (var j = 0; j < users.genreList.length; j++) {
+                    myGenreList.push(users.genreList[j].item_text);
+                }
+                for (var k = 0; k < users.actorsList.length; k++) {
+                    myActorList.push(users.actorsList[k].item_text);
+                }
                 emailList.push(email);
                 var mailOptions = {
                     from: 'findmyshow1@gmail.com',
@@ -21,7 +30,7 @@ module.exports.sendEmail = function (req, res) {
                         style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;"> <tr style="border-collapse:collapse;"> <td align="left" style="padding:0;Margin:0;"> <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"> <tr style="border-collapse:collapse;"> <td width="600" valign="top" align="center" style="padding:0;Margin:0;"> <table style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;border-left:2px solid #959595;border-right:2px solid #959595;border-top:2px solid #959595;border-bottom:2px solid #959595;" width="100%" cellspacing="0" cellpadding="0"> <tr style="border-collapse:collapse;"> <td align="center" style="padding:0;Margin:0;"> <img class="adapt-img" src="https://hisgn.stripocdn.email/content/guids/CABINET_2b6a19284478381997d156b31ea0915a/images/23241540459763459.png" alt=""
                         style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;" width="596"></td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> <table class="es-content" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;"> <tr style="border-collapse:collapse;"> <td align="center" style="padding:0;Margin:0;"> <table class="es-content-body" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;"> <tr style="border-collapse:collapse;"> <td align="left" style="padding:0;Margin:0;padding-left:20px;padding-right:20px;padding-top:30px;"> <table width="100%" cellspacing="0" cellpadding="0"
                         style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"> <tr style="border-collapse:collapse;"> <td width="560" valign="top" align="center" style="padding:0;Margin:0;"> <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"> <tr style="border-collapse:collapse;"> <td align="left" style="padding:0;Margin:0;padding-bottom:5px;">
-          <p><strong>Hello User!!</strong></p><p>Here is a list of popular movies for this week. Go to FindMyShow and find the showtimes</p>
+          <p><strong>Hello ${firstName}!!</strong></p><p>Here is a list of popular movies for this week. Find showtimes of your favourite movie at <a href="http://findmyshow.herokuapp.com">FindMyShow</a> and book your tickets now!!</p>
           <div width=500px height=250px>
             <ul>
             <img src="https://image.tmdb.org/t/p/w500//wrFpXMNBRj2PBiN4Z5kix51XaIZ.jpg"  width=150 height =250px/>
@@ -33,15 +42,17 @@ module.exports.sendEmail = function (req, res) {
             </ul>
           </div>
           <div>
-            <p>
-              Here is the genres you are subscribed to 
-            ${myPreferences}</p></div><p>Thank You</p><p>Have a great weekend</p>
+            <p>Here is the genres you are subscribed to ${myGenreList} and the actors you are subscribed to are ${myActorList} </p>
+          </div>
+          <p>Thank You</p>
+          <p>Have a great weekend</p>
           <table class="es-right" cellspacing="0" cellpadding="0" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:right;"> <tr style="border-collapse:collapse;"> <td width="245" align="left" style="padding:0;Margin:0;"> <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"> <tr style="border-collapse:collapse;"> <td class="es-m-txt-c" align="right" style="padding:0;Margin:0;padding-top:5px;"> <table class="es-table-not-adapt es-social" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"> <tr style="border-collapse:collapse;"> <td valign="top" align="center" style="padding:0;Margin:0;padding-right:10px;"> <img title="Facebook"
                         src="https://hisgn.stripocdn.email/content/assets/img/social-icons/logo-black/facebook-logo-black.png" alt="Fb" width="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;"></td> <td valign="top" align="center" style="padding:0;Margin:0;padding-right:10px;"> <img title="Twitter" src="https://hisgn.stripocdn.email/content/assets/img/social-icons/logo-black/twitter-logo-black.png" alt="Tw" width="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;"></td> <td valign="top" align="center" style="padding:0;Margin:0;padding-right:10px;"> <img title="Instagram" src="https://hisgn.stripocdn.email/content/assets/img/social-icons/logo-black/instagram-logo-black.png" alt="Inst" width="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;"></td> <td valign="top" align="center" style="padding:0;Margin:0;padding-right:10px;"> <img
                         title="Youtube" src="https://hisgn.stripocdn.email/content/assets/img/social-icons/logo-black/youtube-logo-black.png" alt="Yt" width="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;"></td> </tr> </table> </td> </tr> </table> </td> </tr> </table> <!--[if mso]></td></tr></table><![endif]--></td> </tr> </table> </td> </tr> </table> <table class="es-footer" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top;"> <tr style="border-collapse:collapse;"> <td align="center" style="padding:0;Margin:0;"> <table class="es-footer-body" width="600" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#808080;"> <tr
                         style="border-collapse:collapse;"> <td align="left" style="Margin:0;padding-top:20px;padding-bottom:20px;padding-left:20px;padding-right:20px;"> <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"> <tr style="border-collapse:collapse;"> <td width="560" valign="top" align="center" style="padding:0;Margin:0;"> <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"> <tr style="border-collapse:collapse;"> <td align="center" style="padding:0;Margin:0;"> <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:14px;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:150%;color:#FFFFFF;">FindMyShow</p><p
                         style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:14px;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:150%;color:#FFFFFF;">All rights reserved&nbsp;2018</p> </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </div> </body></html>`
                 };
+
                 mailer.sendEmail(mailOptions);
             });
         }
@@ -51,21 +62,25 @@ module.exports.sendEmail = function (req, res) {
 
 function fetchID() {
     var listOfIDs = [];
-    var query = UserPreference.find();
-    return query.exec().then(function (preferences) {
-        for (var x = 0; x < preferences.length; x++) {
-            listOfIDs.push(preferences[x].id);
+    var query = User.find();
+    return query.exec().then(function (users) {
+        for (var x = 0; x < users.length; x++) {
+            listOfIDs.push(users[x]._id);
         }
         return listOfIDs;
     })
 }
 
 function fetchPreferences(myID) {
-    var details = new UserPreference();
-    var query = UserPreference.findById(myID);
-    return query.exec().then(function (preferences) {
-        details.email = preferences.email;
-        details.genreList = preferences.genreList;
+    var details = new User();
+    var query = User.findById(myID);
+    return query.exec().then(function (users) {
+        details.email = users.email;
+        details.genreList = users.genreList;
+        details.actorsList = users.actorsList;
+        details.firstname = users.firstname;
+        details.lastname = users.lastname;
+        details.city = users.city;
         return details;
     })
 }
