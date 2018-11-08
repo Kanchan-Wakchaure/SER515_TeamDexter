ef5786e6b4f9189e785f7c699932d2cee017f121
shipabhat92 <sgbhat2@asu.edu>
2018-11-08 10:45:30 -0700

    feat (user preference) : user story:34, task154 update service uri based and model based on changes made in backend

--- Client/src/app/Services/preference.service.ts
+++ Client/src/app/Services/preference.service.ts
@@ -10,7 +10,7 @@ import {
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json',
+    'Content-Type': 'application/json',
     'Authorization': 'my-auth-token'
   })
 };
@@ -21,23 +21,23 @@ export class PreferenceService {
   public preference: Preference;
   public var;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getPreferencesById(id: number) {
 
-    return this.http.get("http://localhost:4241/preferences/" +id, {
+    return this.http.get("http://localhost:4241/preferences/" + id, {
       responseType: "json"
     });
   }
 
   getPreferencesByEmail(email: string) {
-    return this.http.get("http://localhost:4241/preferences/?email=" +email, {
+    return this.http.get("http://localhost:4241/preferences/?email=" + email, {
       responseType: "json"
     });
   }
 
   updatePreferences(id: number, preferences: Preference) {
-    this.http.put("http://localhost:4241/preferences/"+ id, 
+    this.http.put("http://localhost:4241/preferences/" + id,
       preferences, httpOptions).subscribe(results => this.var = results);
   }
 
