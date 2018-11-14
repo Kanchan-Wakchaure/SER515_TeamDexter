9c8f12112f8e0dd01e9c617785ac251b15786926
vivek-faldu <vfaldu@asu.edu>
2018-11-13 23:52:07 -0700

    Feat: (User Authentication added) User Story: 134 Task: 172

--- Server/routes/preferences.js
+++ Server/routes/preferences.js
@@ -1,11 +1,11 @@
 const User = require('../models/User');
-
+const {verifyToken} = require('../middlewares/verifyToken')
 var express = require("express");
 var router = express.Router();
 
 //get with id
-router.get("/:id", function (req, res, next) {
-    User.findById(req.params.id, (err, user) => {
+router.get("/",verifyToken, function (req, res, next) {
+    User.findById(req.userid, (err, user) => {
         if (err) {
             next(err);
         } else {
@@ -16,27 +16,29 @@ router.get("/:id", function (req, res, next) {
             };
             userPreference.genreList = user.genreList;
             userPreference.languageList = user.languageList;
-            userPreference.actorsList = user.actorsList;                        
+            userPreference.actorsList = user.actorsList;
             return res.json(userPreference);
         }
     })
 });
 
 //update user preference
-router.put('/:id', function (req, res, next) {
-    User.findById(req.params.id, (err, user) => {
+router.put('/',verifyToken, function (req, res, next) {
+    //console.log(req.userid)
+    User.findById(req.userid, (err, user) => {
         if (!user) {
             next(new Error("User not found"));
         } else {
             user.genreList = req.body.genreList;
             user.languageList = req.body.languageList;
             user.actorsList = req.body.actorsList;
+            console.log(user.genreList,user.languageList,user.actorsList)
             user.save().then(respose => {
-                    res.status(200).json({
-                        'preference': 'updated successfully'
-                    });
-                })
-                .catch(err => {                    
+                res.status(200).json({
+                    'preference': 'updated successfully'
+                });
+            })
+                .catch(err => {
                     next(err);
                 });
         }
--- Server/routes/showtimes.js
+++ Server/routes/showtimes.js
@@ -8,10 +8,10 @@ var {
 router.get("/:id", function (req, res, next) {
     var date = req.query.date;
     var city = req.query.city
-    if (typeof(date) === 'undefined'){
+    if (typeof (date) === 'undefined') {
         date = 1
     }
-    if (typeof(city) === 'undefined'){
+    if (typeof (city) === 'undefined') {
         city = '1901'
     }
     else {
